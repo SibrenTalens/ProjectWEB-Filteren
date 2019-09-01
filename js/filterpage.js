ae745ae8b2a651eea27c12625a707ef979955faa
JKoenen99 <jap.koenen@hotmail.nl>
2019-09-01 20:46:53 +0200

    Style update & cards update

---
+++ js/filterpage.js
@@ -0,0 +1,41 @@
+var searchbox = document.querySelector('input.search-box');
+var searchbutton = document.querySelector('button.search-button');
+var spanicon = document.querySelector('span.fa');
+
+searchbutton.addEventListener('click', function() {
+    searchbutton.classList.add('active');
+    searchbox.classList.add('active');
+    searchbox.focus();
+    spanicon.classList.replace('fa-search', 'fa-js-square');
+    console.log('actie');
+});
+
+var main = document.querySelector('main');
+
+main.addEventListener('click', function() {
+  spanicon.classList.replace('fa-js-square', 'fa-search');
+  searchbox.classList.remove('active');
+  searchbutton.classList.remove('active');
+  console.log('klik weg');
+});
+
+
+function myFunction() {
+  // Declare variables
+  var input, filter, ul, li, h2, i, txtValue;
+  input = document.querySelector('input.search-box');
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("mySection");
+  li = ul.getElementsByTagName('li');
+
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < li.length; i++) {
+    h2 = li[i].getElementsByTagName("h2")[0];
+    txtValue = h2.textContent || h2.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
